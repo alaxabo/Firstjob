890014a800922aec97f90bdee5a3b9d609255551
Tuan anh Tran <trantuananh2008@gmail.com>
2018-07-02 15:44:07 +0700

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by ANHTT-D1 on 7/2/18.
+//  Copyright © 2018 anhtt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
